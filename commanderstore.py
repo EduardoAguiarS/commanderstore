a6e0286c92821562f1be73067dd35cd5f59ad8fa
Eduardo Aguiar <edu.pessoal@outlook.com>
2023-08-10 09:55:37 -0300

    categories feature

--- commanderstore.py
+++ commanderstore.py
@@ -10,6 +10,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 app.app_context().push()
 
+
 # Database models
 class Usuario(db.Model):
     __tablename__ = "usuario"
@@ -26,10 +27,40 @@ class Usuario(db.Model):
     def __repr__(self):
         return f'Usuario {self.nome}'
 
+
+class Categoria(db.Model):
+    __tablename__ = "categoria"
+    id = db.Column('cat_id', db.Integer, primary_key=True)
+    nome = db.Column('cat_nome', db.String(256))
+    desc = db.Column('cat_desc', db.String(256))
+
+    def __init__(self, nome, desc):
+        self.nome = nome
+        self.desc = desc
+
+
+class Anuncio(db.Model):
+    __tablename__ = "anuncio"
+    id = db.Column('anu_id', db.Integer, primary_key=True)
+    nome = db.Column('anu_nome', db.String(256))
+    desc = db.Column('anu_desc', db.String(256))
+    preco = db.Column('anu_preco', db.Float)
+    cat_id = db.Column('cat_id', db.Integer, db.ForeignKey("categoria.cat_id"))
+    usr_id = db.Column('usr_id', db.Integer, db.ForeignKey("usuario.usr_id"))
+
+    def __init__(self, nome, desc, preco, cat_id, usr_id):
+        self.nome = nome
+        self.desc = desc
+        self.preco = preco
+        self.cat_id = cat_id
+        self.usr_id = usr_id
+
+
 # Link to css
 @app.route('/static/css/<path:path>')
 def send_css(path):
     return send_from_directory('static/css', path)
+
 
 # Routes
 # Home page
@@ -37,14 +68,16 @@ def send_css(path):
 def index():
     return render_template('index.html', usuarios=Usuario.query.all())
 
+
 # Sign up page
 @app.route('/signup')
 def signup():
     return render_template('signup.html', title='Cadastro')
 
-# Sign up success page
-@app.route('/signupsuccess', methods=['POST'])
-def signupsuccess():
+
+# New user
+@app.route('/usuario/novo', methods=['POST'])
+def criarusuario():
     usuario = Usuario(
         request.form.get('name'),
         request.form.get('email'),
@@ -54,22 +87,66 @@ def signupsuccess():
     db.session.commit()
     return redirect(url_for('signin'))
 
+
 # Sign in page
 @app.route('/signin')
 def signin():
     return render_template('signin.html', title='Entrar')
 
-@app.route('/produtos')
-def produtos():
-    return render_template('produtos.html', title='Produtos')
+
+# Categories page
+@app.route('/config/categorias')
+def categorias():
+    return render_template('categorias.html', title='Categorias', categorias=Categoria.query.all())
+
+
+# New category
+@app.route('/categorias/novo', methods=['POST'])
+def categorias_novo():
+    categoria = Categoria(
+        request.form.get('name'),
+        request.form.get('desc')
+    )
+    db.session.add(categoria)
+    db.session.commit()
+    return redirect(url_for('categorias'))
+
+
+# Products page
+@app.route('/anuncios')
+def anuncios():
+    return render_template(
+        'anuncios.html',
+        title='Anuncios',
+        anuncios=Anuncio.query.all(),
+        categorias=Categoria.query.all()
+    )
+
+
+# New product
+@app.route('/anuncios/novo', methods=['POST'])
+def anuncios_novo():
+    anuncio = Anuncio(
+        request.form.get('name'),
+        request.form.get('desc'),
+        request.form.get('preco'),
+        request.form.get('cat_id'),
+        request.form.get('usr_id')
+    )
+    db.session.add(anuncio)
+    db.session.commit()
+    return redirect(url_for('anuncios'))
+
 
 @app.route('/produtos/<id>')
 def produto(id):
     return render_template('produto.html', id=id, title='Produto')
 
+
 @app.route('/favoritos')
 def favoritos():
     return render_template('favoritos.html', title='Favoritos')
+
 
 # Run app
 if __name__ == 'commanderstore':
